b7172a6755b1b9be73977c7887919f2edb2837ab
Petrea <77539633+neapetrica@users.noreply.github.com>
2021-01-21 12:49:13 +0200

    Add files via upload

---
+++ script.js
@@ -0,0 +1,10 @@
+ function da(){
+ 	Swal.fire({
+  title: 'Da!',
+  text: 'Una cu de toate!',
+  imageUrl: 'shaorma.jpg',
+  imageWidth: 400,
+  imageHeight: 200,
+  imageAlt: 'Custom image',
+})
+ }
